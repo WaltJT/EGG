8d9497bcefc74987f8496779b09732c2ad3e7f01
WJTerranova <wjterranova@gmail.com>
2023-04-07 13:56:01 -0300

    Modificaciones

---
+++ ARG4.0 Ejercicio1_Dia2.java
@@ -0,0 +1,14 @@
+public class Ejercicio1 {
+
+public static void name(String[]args) {
+        
+    int Num1 = 10;
+    int Num2 = 20;
+    
+    System.out.println(suma =Num1+Num2);
+    
+    System.out.println(+ suma);
+
+    }
+   
+}
---
+++ Prueba1/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        int num1 = 40;
+        int num2 = 180;
+    
+        System.out.println(num1+num2);
+    
+
+        }
+
+
+    }
